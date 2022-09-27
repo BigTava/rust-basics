2cad899c0e86e1876431025154ca2a2f665f04a8
Tiago Tavares <bigtavadev@gmail.com>
2022-09-27 13:09:59 +0100

    feat: calculates arithmetic operations

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
 fn main() {
-    let mut x = 10;
-
+    let mut x: u8 = 255;
     println!("x is {}", x);
     x = 20;
     println!("x is {}", x);
+
+    let y: f32 = 10.012312321312312123213213;
+    println!("y is {}", y);
+
+    /* Arithmetic operations */
+    let a = 10;
+    let b = 3;
+    let c = a + b;
+    println!("c is {}", c);
+
+    let d = a - b;
+    println!("d is {}", d);
+
+    let e = a * b;
+    println!("e is {}", e);
+
+    let f = a / b;
+    println!("f is {}", f);
+
+    let g = a % b;
+    println!("g is {}", g);
+
+    let h = 10.0;
+    let j = 3.0;
+    let i = h / j;
+    println!("i is {}", i);
+
+    let k = a as f64 / j;
+    println!("k is {}", k);
 }
