191950738aa5deacec8df1758b899423bcfe4469
Tiago Tavares <bigtavadev@gmail.com>
2022-10-20 15:16:56 +0100

    learns references

--- src/intermediate.rs
+++ src/intermediate.rs
@@ -1,5 +1,109 @@
 fn main() {
-    ownership();
+    // ownership();
+    // ref_ownership();
+    // reference();
+    // slices();
+    challenge();
+}
+
+/* REFERENCES */
+fn trim_spaces(s: &str) -> &str{
+    let mut last_front_space: usize = 0;
+    for (index, character) in s.chars().enumerate() {
+        if !character.is_ascii_whitespace() {
+            last_front_space = index;
+            break;
+        }
+    }
+
+    let mut first_rear_space: usize = 0;
+    for (index, character) in s.chars().rev().enumerate() {
+        if !character.is_ascii_whitespace() {
+            first_rear_space = s.len() - index;
+            break;
+        }
+    }
+    
+    &s[last_front_space..first_rear_space]
+}
+
+fn challenge()  {
+    let test1 = "We need more space.";
+    assert_eq!(trim_spaces(test1), "We need more space.");
+    
+    let test2 = String::from("   There's space in front.");
+    assert_eq!(trim_spaces(&test2), "There's space in front.");
+    
+    let test3 = String::from("There's space to the rear. ");
+    assert_eq!(trim_spaces(&test3[..]), "There's space to the rear.");   
+    
+    let test4 = "  We're surrounded by space!    ";
+    assert_eq!(trim_spaces(test4), "We're surrounded by space!");
+    
+    let test5 = "     ";
+    assert_eq!(trim_spaces(test5), "");
+    
+    let test6 = "";
+    assert_eq!(trim_spaces(test6), "");
+    
+    let test7 = " x🚀xx ";
+    assert_eq!(trim_spaces(test7), "x🚀xx");
+    println!("Tests passed!");
+}
+
+fn ref_ownership() {
+    let mut rocket_fuel = String::from("RP-1");
+    let length = ref_process_fuel(&mut rocket_fuel);
+    println!("rocket_fuel is {} and length is {}", rocket_fuel, length);
+}
+
+fn ref_process_fuel(propellant: &mut String) -> usize {
+    println!("processing propellant {}...", propellant);
+    propellant.push_str(" is highly flammable!");
+    let length = propellant.len();
+    length
+}
+
+fn slices() {
+    let message = String::from("Greetings from Earth!");
+    println!("message is {}", message);
+
+    // should be careful about emojis or special characters that span multiple bytes
+    let last_word = &message[15..];
+    println!("last_word is {}", last_word);
+
+    let planets = [1,2,3,4,5,6,7,8];
+    let inner_planets: &[i32] = &planets[..4];
+    println!("inner_planets are {:?}", inner_planets);
+
+    let first_word = get_first_word(&message[10..]);
+    // let first_word = get_first_word(&message[10..]);
+    // impossible to pass slice of message as a reference slice is different from a string (it borrows from the heap)
+    // a reference string borrows from a variable  that owns a string in the heap
+    // it works if the argument in get_first_word is s: &str
+    println!("first_word is {}", first_word);
+}
+
+fn reference() {
+    let rocket_fuel = produce_fuel();
+    println!("rocket_fuel is {}", rocket_fuel)
+}
+
+fn produce_fuel() -> String {
+    let new_fuel = String::from("RP-1");
+    new_fuel
+}
+
+fn get_first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (index, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..index]; // found a space!
+        }
+    }
+
+    &s // no spaces found; input is a single word
 }
 
 /* OWNERSHIP */
@@ -45,3 +149,5 @@ fn process_fuel(propellant: String) -> String {
     // 2nd transfers ownership back to the main function
     propellant
 }
+
+
