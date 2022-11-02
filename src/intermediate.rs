cc5890a9ed8895d1ef1fd45d2b860e2a0db2c992
Tiago Tavares <bigtavadev@gmail.com>
2022-11-02 14:06:55 +0000

    learns structs

--- src/intermediate.rs
+++ src/intermediate.rs
@@ -17,7 +17,110 @@ fn main() {
     // cmd();
     // read_files();
     // write_files();
-    challenge_io_solution();
+    // challenge_io_solution();
+    // create_struct();
+    // create_tuple_struct();
+    challenge_structs();
+}
+
+/* STRUCTS */
+#[derive(Debug)] // allows printing object
+#[derive(Clone)] // allows cloning object
+struct Shuttle {
+    name: String, 
+    crew_size: u8, 
+    propellant: f64
+}
+
+impl Shuttle {
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    fn add_fuel(&mut self, gallons: f64) {
+        self.propellant += gallons;
+    }
+
+    fn new(name: &str) -> Shuttle {
+        Shuttle {
+            name: String::from(name),
+            crew_size: 7,
+            propellant:0.0
+        }
+    }
+}
+
+
+
+fn create_struct() {
+    /*let mut vehicle = Shuttle {
+        name: String::from("Endeavour"),
+        crew_size: 7,
+        propellant: 835958.0
+    };*/
+    let mut vehicle = Shuttle::new("Endeavour2");
+
+    /*let mut vehicle2 = Shuttle {
+        ..vehicle.clone()
+    };*/
+    let mut vehicle2 = Shuttle::new("Discovery");
+
+    vehicle.crew_size = 6;
+    println!("vehicle is {:?}", vehicle);
+    println!("vehicle2 is {:?}", vehicle2);
+
+    let vehicle_name = vehicle.get_name();
+    println!("vehicle_name is {}", vehicle_name);
+
+    println!("propellant is {}", vehicle.propellant);
+    vehicle.add_fuel(1000.0);
+    println!("propellant is {}", vehicle.propellant);
+}
+struct Color(u8,u8,u8); //RGB
+struct Point(u8,u8,u8); // XYZ
+
+fn create_tuple_struct() {
+    let red = Color(255, 0, 0);
+    println!("First value is {}", red.0);
+
+    let coord = Point(4,5,6);
+    let y = get_y(coord);
+    println!("y is {}", y);
+}
+
+fn get_y(p: Point) -> u8 {
+    p.1
+}
+
+struct Rectangle {
+    width: f64, 
+    height: f64
+}
+
+impl Rectangle {
+    fn get_area(&self) -> f64 {
+        self.width*self.height
+    }
+
+    fn scale(&mut self, scaler: f64) {
+        self.width *= scaler;
+        self.height *= scaler;
+    }
+
+    fn new(_width: f64, _height: f64) -> Rectangle {
+        Rectangle {
+            width: _width,
+            height: _height,
+        }
+    }
+}
+
+fn challenge_structs() {
+    let mut rect = Rectangle::new(1.2, 3.4);
+    assert_eq!(rect.get_area(), 4.08);
+    rect.scale(0.5);
+    assert_eq!(rect.get_area(), 1.02);
+    println!("Tests passed!");
 }
 
 /* INPUTS AND OUTPUTS */
