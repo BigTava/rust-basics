ef219765e17dbd07445bdd8fa7126c0bf1d2bfab
Tiago Tavares <bigtavadev@gmail.com>
2022-12-27 16:39:05 +0000

    learns errors handling

--- src/advanced.rs
+++ src/advanced.rs
@@ -1,5 +1,8 @@
 use std::any;
 use std::fmt;
+use std::fs;
+use std::io;
+use rand::prelude::*;
 
 fn main() {
     // simple_trait();
@@ -17,7 +20,106 @@ fn main() {
     // option();
     // matching_option();
     // if_let();
-    challenge_enums();
+    // challenge_enums();
+    // unrecoverable_errors();
+    // result_enum();
+    // recover_from_errors();
+    // propagating_errors();
+    challenge_errors();
+}
+
+/* ERROR HANDLING */
+fn unrecoverable_errors() {
+    // panic!("Houston, we've had a problem!");
+    let countdown = [5,4,3,2,1,0];
+
+    for count in countdown.iter() {
+        println!{"T-minus {}", count};
+        let x = 1 / count; 
+    }
+}
+
+fn result_enum() {
+    let contents = fs::read_to_string("the_ultimate_question.txt").expect("Nobody knows the ultimate question!");
+    println!("contents is : {:?}", contents);
+}
+
+fn recover_from_errors() {
+    let result = fs::read_to_string("the_ultimate_question.txt");
+
+    /*
+    let contents = match result {
+        Ok(message) => message,
+        Err(error) => String::from("Nobody knows the ultimate question!")
+    };
+
+    println!("contents is : {:?}", contents);
+    */
+
+    let contents = match result {
+        Ok(message) => message,
+        Err(error) =>  match error.kind() {
+            io::ErrorKind::NotFound => String::from("File not found."),
+            io::ErrorKind::PermissionDenied => String::from("Permission denied."),
+            _ => panic!("Another type of error: {:?}", error)
+        }
+    };
+
+    println!("contents is : {:?}", contents);
+}
+
+fn read_and_combine(f1: &str, f2: &str) -> Result<String, io::Error> {
+    let mut s1 = fs::read_to_string(f1)?; // shorthand error handling
+
+    // exactly the same as the previous
+    let s2 = match fs::read_to_string(f2) {
+        Ok(s) => s,
+        Err(e) => return Err(e)
+    };
+
+    s1.push('\n');
+    s1.push_str(&s2);
+    Ok(s1)
+}
+
+fn challenge_errors() {
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("I'm thinking of a number between 1 and 100...");
+    println!("Guess the number:");
+    loop {
+        let mut buffer = String::new();
+        let guess = match io::stdin().read_line(&mut buffer){
+            Ok(_) => match buffer.trim().parse::<u32>() {
+                Ok(value) => value, // success
+                Err(_) => {
+                    println!("\nFailed to parse input. Guess again:");
+                    continue
+                }
+            }
+            Err(_) => {
+                println!("\nFailed to read input. Guess again:");
+                continue
+            }
+        };
+
+        if guess > secret_number {
+            println!("\n{} is too high! Guess lower:", guess);
+        } else if guess < secret_number {
+            println!("\n{} is too low! Guess higher:", guess);
+        } else {
+            println!("\nYou got it! The secret number was {}.", secret_number);
+            break;
+        }
+    }   
+}
+
+fn propagating_errors() {
+    let result = read_and_combine("./src/assets/planetsd.txt", "./src/assets/dwarf_planets.txt");
+    match result {
+        Ok(s) => println!("result is...\n{}",s),
+        Err(e) => println!("There was an error: {}",e),
+    }
 }
 
 /* ENUMS */
@@ -50,7 +152,6 @@ fn define_enums() {
 
     let perimeter = my_shape.get_perimeter();
     println!("perimeter is {}", perimeter);
-
 }
 
 fn match_expression() {
